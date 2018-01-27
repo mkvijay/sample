bbb64613393d3594439e1adbfb149b5821be13f7
mkvijay <vijay45kmar@gmail.com>
2018-01-27 17:56:15 -0500

    add notifier library

---
+++ vars/notifier.groovy
@@ -0,0 +1,40 @@
+vars/sendNotifications.groovy
+#!/usr/bin/env groovy
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def message = "${subject} (${env.BUILD_URL})"
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESS') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+
+  // Send notifications
+  slackSend (channel: '#work-test', color: colorCode, message: summary)
+
+  hipchatSend (color: color, notify: true, message: summary)
+
+  emailext (
+      to: 'vijay45kmar@gmail.com',
+      subject: subject,
+      body:message, 
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+}
