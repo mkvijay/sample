af9588884e57f275efb52ef404c6ee349268524f
mkvijay <vijay45kmar@gmail.com>
2018-01-28 22:34:49 -0500

    Update notifier.groovy

--- vars/notifier.groovy
+++ vars/notifier.groovy
@@ -1,4 +1,3 @@
-vars/sendNotifications.groovy
 #!/usr/bin/env groovy
 
 /**
@@ -6,26 +5,23 @@ vars/sendNotifications.groovy
  */
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
-  buildStatus = buildStatus ?: 'SUCCESS'
+  buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
   // Default values
-  def colorName = 'RED'
+  def color = 'RED'
   def colorCode = '#FF0000'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def message = "${subject} (${env.BUILD_URL})"
+  def summary = "${subject} (${env.BUILD_URL})"
 
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESS') {
+  } else if (buildStatus == 'SUCCESSFUL') {
     color = 'GREEN'
     colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
   }
 
   // Send notifications
-  slackSend (channel: '#work-test', color: colorCode, subject: message)
+  slackSend (channel: '#work-test', color: colorCode, message: summary)
 }
