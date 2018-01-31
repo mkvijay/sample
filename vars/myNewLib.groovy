0b3ac8a8565f105c0c633c0df092f650ef7183c3
mkvijay <vijay45kmar@gmail.com>
2018-01-30 23:29:15 -0500

    Update myNewLib.groovy

--- vars/myNewLib.groovy
+++ vars/myNewLib.groovy
@@ -9,7 +9,7 @@ def call(Map parameters = [:]) {
   def message = parameters.get('message')
   def build_status = currentBuild.currentResult
   
-  if (build_status == null) {
+  if (build_status != null) {
     color = 'GREEN'
     colorCode = '#00FF00'
   } else {
