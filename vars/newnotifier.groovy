df14fc1097ca7addf034e41143df2528d5deb09d
mkvijay <vijay45kmar@gmail.com>
2018-01-30 22:05:11 -0500

    first test

---
+++ vars/newnotifier.groovy
@@ -0,0 +1,14 @@
+#!usr/bin/groovy
+def call(Map parameters = [:]) {
+  // build status of null means successful
+  echo 'notifier parameters: ' + parameters
+
+  def environment = parameters.get('environment')
+  def slack_channel = parameters.get('slack_channel')
+  def email = parameters.get('email')
+  def message = parameters.get('message')
+
+  if (slack_channel != null) {
+      slackSend (channel: slack_channel, subject: "${env.service} Pipeline Notification" )
+  }
+}
