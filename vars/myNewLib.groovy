e5a097c3140ac3a0073036adc1581fde0929ebe7
mkvijay <vijay45kmar@gmail.com>
2018-01-30 23:08:38 -0500

    Update myNewLib.groovy

--- vars/myNewLib.groovy
+++ vars/myNewLib.groovy
@@ -9,6 +9,6 @@ def call(Map parameters = [:]) {
   def message = parameters.get('message')
   def build_status = parameters.get('build_status')
 
-  if (buildStatus != null) {
+  if (build_status != null) {
   slackSend (channel: slack_channel, subject: "${env.service} Pipeline Notification" )
 }
