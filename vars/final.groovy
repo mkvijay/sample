e0a6ddb0af8159bf8f0ba38138011090b6839419
mkvijay <vijay45kmar@gmail.com>
2018-01-30 23:47:39 -0500

    final testing

---
+++ vars/final.groovy
@@ -0,0 +1,23 @@
+#!usr/bin/groovy
+def call(Map parameters = [:]) {
+  // build status of null means successful
+  echo 'myNewLib parameters: ' + parameters
+
+  def environment = parameters.get('environment')
+  def slack_channel = parameters.get('slack_channel')
+  def email = parameters.get('email')
+  def message = parameters.get('message')
+  
+  if (slack_channel != null) {
+  slackSend (channel: slack_channel, message: "testing slack ${env.BUILD_URL}", subject: "${env.service} Pipeline Notification" )
+  }
+  if (email != null) {
+      emailext (
+        to: email,
+        subject: "${env.service} Pipeline Notification!",
+        body: "${env.service} pipeline deploy status, please take a look:  ${env.BUILD_URL}",
+        recipientProviders: [[$class: 'DevelopersRecipientProvider']],
+        replyTo: 'no.reply.jenkins@corp.local'
+      )
+  }
+}
